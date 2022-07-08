3ed253baaa968ddd284ff370a0f8e5cd71e7356f
happyeveryone96 <happyeveryone@naver.com>
2022-07-08 10:25:15 +0900

    Add: 마우스를 누르고 있는 경우 숫자 변경 기능 추가

--- react-ts-toy-project/src/Counter.tsx
+++ react-ts-toy-project/src/Counter.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useCounterState, useCounterDispatch } from "./Context";
 import styled from "styled-components";
 
@@ -10,12 +11,48 @@ function Counter() {
   const decreaseCount = (count: number) =>
     dispatch({ type: "DECREMENT", count });
 
+  const [minusPressed, setMinusPressed] = useState(false);
+  const [plusPressed, setPlusPressed] = useState(false);
+
+  const handleMouseUp = () => {
+    setMinusPressed(false);
+    setPlusPressed(false);
+  };
+
+  const minusMouseDown = () => {
+    decreaseCount(1);
+    setMinusPressed(true);
+  };
+  const plusMouseDown = () => {
+    increaseCount(1);
+    setPlusPressed(true);
+  };
+
+  useEffect(() => {
+    if (minusPressed) {
+      const timer = setInterval(() => {
+        decreaseCount(1);
+      }, 200);
+      return () => clearInterval(timer);
+    }
+    if (plusPressed) {
+      const timer = setInterval(() => {
+        increaseCount(1);
+      }, 200);
+      return () => clearInterval(timer);
+    }
+  });
+
   return (
     <Container>
       <Count>count: {state.count}</Count>
       <Buttons>
-        <Button onClick={() => decreaseCount(1)}>-1</Button>
-        <Button onClick={() => increaseCount(1)}>+1</Button>
+        <Button onMouseUp={handleMouseUp} onMouseDown={minusMouseDown}>
+          -1
+        </Button>
+        <Button onMouseUp={handleMouseUp} onMouseDown={plusMouseDown}>
+          +1
+        </Button>
       </Buttons>
     </Container>
   );
